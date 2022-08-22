c7a092c0bfa1077bcacb10594b07ce6f909b9911
Miguel Ângelo Ribeiro <maribeiro@f3m.pt>
2022-08-22 14:06:19 +0100

    unnecessary admin permissions

--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
-﻿using Squirrel;
+﻿using Nancy.Hosting.Self;
+using Squirrel;
 using System;
 using System.Net.Http.Headers;
+using System.ServiceModel;
 using System.Web.Http;
 using System.Web.Http.Cors;
 using System.Web.Http.SelfHost;
@@ -9,44 +11,33 @@ namespace SmartCardReader
 {
     class Program
     {
-        public static void CheckForUpdates()
-        {
-            try
-            {
-                using (var manager = new UpdateManager("https://github.com/majvr93/SmartCardReader"))
-                {
-                    manager.UpdateApp();
-                }
-
-            } 
-            catch (Exception ex)
-            {
-                Console.WriteLine("Failed to check updates!");
-            }
-           
-        }
-
+       
         static void Main(string[] args)
         {
-            CheckForUpdates();
-
             HttpSelfHostServer server = null;
             try
-            {
-                // Set up server configuration
-                var config = new HttpSelfHostConfiguration(new Uri("http://localhost:8000"));
-                config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("application/json"));               
+            {                
+                // Set up server configuration    
+                var config = new HttpSelfHostConfiguration(new Uri("http://localhost:8000"))
+                {
+                    // to avoid `(413) Request Entity Too Large` exceptions
+                    MaxReceivedMessageSize = int.MaxValue,
+                    MaxBufferSize = int.MaxValue
+                };
+                config.HostNameComparisonMode = HostNameComparisonMode.Exact;
+
+                config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("application/json"));
                 config.Routes.MapHttpRoute(
                     name: "ActionApi",
                     routeTemplate: "api/{controller}/{action}/{id}",
                     defaults: new { id = RouteParameter.Optional });
-
                 config.EnableCors(new EnableCorsAttribute("*", "*", "*"));
-                
                 // Create server
                 server = new HttpSelfHostServer(config);
                 // Start listening
                 server.OpenAsync().Wait();
+                
+
                 Console.WriteLine("Listening on http://localhost:8000");
                 Console.ReadLine();
             }
